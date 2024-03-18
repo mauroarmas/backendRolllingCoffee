fed31b408a20db5376beeb5c08841707dc733ea7
mauroarms <mauro.armas14@gmail.com>
2024-03-18 12:02:42 -0300

    se agregó express al proyecto

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import express from 'express'
+
 console.log("Hola mundo")
+
+//1° Configurar un puerto
+
+//2° Configurar un middleware (Funciones que se ejecuta antes de llegar a las rutas)
+
+//3° Configuracion de las rutas 
