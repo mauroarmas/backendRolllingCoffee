f9058066b04c7e94c578206832136061a4b62c49
mauroarms <mauro.armas14@gmail.com>
2024-03-21 00:39:21 -0300

    Se agregó las funciones para listar por id, por infusion y editar

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -82,3 +82,21 @@ export const listarProductosPorInfusion = async (req, res) => {
     });
   }
 }
+
+export const editarProductoPorId = async (req, res) => {
+  try{
+    //
+
+    const productoEncontrado = await Producto.findById(req.params.id)
+    if(!productoEncontrado){
+      return res.status(404).json({message: "Producto no encontrado"})
+    }
+      await Producto.findByIdAndUpdate(req.params.id, req.body)
+      res.status(200).json({message: "Producto Editado"})
+    
+
+  }catch(error){
+    console.error(error);
+    res.status(500).json({message: "Ocurrio un error al editar el producto"})
+  }
+}
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { listarProductos, agregarProducto, listarProductoPorId, listarProductosPorInfusion } from "../controllers/productos.controllers.js";
+import { listarProductos, agregarProducto, listarProductoPorId, listarProductosPorInfusion, editarProductoPorId } from "../controllers/productos.controllers.js";
 
 const router = Router();
 router.route("/productos").get(listarProductos).post(agregarProducto)
 router.route("/productos/:id").get(listarProductoPorId)
+router.route("/productos/editar/:id").put(editarProductoPorId)
 router.route("/productos/categoria/infusion").get(listarProductosPorInfusion)
 
 export default router;
