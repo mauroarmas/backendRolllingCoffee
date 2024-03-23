45ca8a352bc5bede5e13eedbb0b37d9ef76c996d
mauroarms <mauro.armas14@gmail.com>
2024-03-23 16:52:10 -0300

    Se agrego la funcionalidad de borrar producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,21 +1,20 @@
 import Producto from "../database/models/producto.js";
 
 export const listarProductos = async (req, res) => {
-  try{
+  try {
     // *Buscar Productos
     const resultado = await Producto.find();
 
     // devolver los productos
-    res.status(200).json(resultado)
-
-  }catch(error){
-    console.error(error)
+    res.status(200).json(resultado);
+  } catch (error) {
+    console.error(error);
     res.status(404).json({
-      message : "No se pudo obtener los datos"
-    })
+      message: "No se pudo obtener los datos",
+    });
   }
   console.log("Hola mundo desde el controlador");
-  res.send("listando productos");
+
 };
 
 export const agregarProducto = async (req, res) => {
@@ -45,58 +44,66 @@ export const agregarProducto = async (req, res) => {
 };
 
 export const listarProductoPorId = async (req, res) => {
-  try{
+  try {
     //extraer producto id
-    console.log(req.params)
+    console.log(req.params);
 
     //buscar el producto en la BD
-    const productoEncontrado = await Producto.findById(req.params.id)
+    const productoEncontrado = await Producto.findById(req.params.id);
 
     //responder con el producto
-    res.status(200).json(productoEncontrado)
-
-  }catch(error){
-    console.error(error)
+    res.status(200).json(productoEncontrado);
+  } catch (error) {
+    console.error(error);
     res.status(400).json({
       mensage: "El producto no se encontro",
     });
   }
-}
+};
 
 export const listarProductosPorInfusion = async (req, res) => {
-  try{
+  try {
     //extraer producto id
-    console.log(req.params)
+    console.log(req.params);
 
     //buscar el producto en la BD
-    const productosEncontrados = await Producto.find({categoria: "Infusión"})
+    const productosEncontrados = await Producto.find({ categoria: "Infusión" });
     //const productoEncontrado = await Producto.findById(req.params)
 
     //responder con el producto
-    res.status(200).json(productosEncontrados)
-
-  }catch(error){
-    console.error(error)
+    res.status(200).json(productosEncontrados);
+  } catch (error) {
+    console.error(error);
     res.status(404).json({
       mensage: "El producto no se encontro",
     });
   }
-}
+};
 
 export const editarProductoPorId = async (req, res) => {
-  try{
-    //
-
-    const productoEncontrado = await Producto.findById(req.params.id)
-    if(!productoEncontrado){
-      return res.status(404).json({message: "Producto no encontrado"})
+  try {
+    const productoEncontrado = await Producto.findById(req.params.id);
+    if (!productoEncontrado) {
+      return res.status(404).json({ message: "Producto no encontrado" });
     }
-      await Producto.findByIdAndUpdate(req.params.id, req.body)
-      res.status(200).json({message: "Producto Editado"})
-    
-
-  }catch(error){
+    await Producto.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({ message: "Producto Editado" });
+  } catch (error) {
     console.error(error);
-    res.status(500).json({message: "Ocurrio un error al editar el producto"})
+    res.status(500).json({ message: "Ocurrio un error al editar el producto" });
   }
-}
+};
+
+export const borrarProductoPorId = async (req, res) => {
+  try {
+    const productoEncontrado = await Producto.findById(req.params.id);
+    if (!productoEncontrado) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+    await Producto.findByIdAndDelete(req.params.id, req.body);
+    res.status(200).json({ message: "Producto Borrado" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Ocurrio un error al editar el producto" });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { listarProductos, agregarProducto, listarProductoPorId, listarProductosPorInfusion, editarProductoPorId } from "../controllers/productos.controllers.js";
+import { listarProductos, agregarProducto, listarProductoPorId, listarProductosPorInfusion, editarProductoPorId, borrarProductoPorId } from "../controllers/productos.controllers.js";
 
 const router = Router();
 router.route("/productos").get(listarProductos).post(agregarProducto)
-router.route("/productos/:id").get(listarProductoPorId)
-router.route("/productos/editar/:id").put(editarProductoPorId)
+router.route("/productos/:id").get(listarProductoPorId).delete(borrarProductoPorId).put(editarProductoPorId)
 router.route("/productos/categoria/infusion").get(listarProductosPorInfusion)
 
 export default router;
